72e1baf4d596a5b374cdf3d1ecc5612b553625c4
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2025-08-01 00:53:26 +0000

    remove deprecated code

--- pymetrica/main.py
+++ pymetrica/main.py
@@ -1,5 +1,4 @@
 from pymetrica.metric_calculators import aloc
-from .deprecated_all import deprecated_all
 from .run_all import run_all
 from .status import status
 from .codebase_parser import base_stats
@@ -11,7 +10,6 @@ def main(): ...
 
 
 main.add_command(status)
-main.add_command(deprecated_all)
 main.add_command(run_all)
 main.add_command(base_stats)
 main.add_command(aloc)
