f38dc20c3839ce54db282b59dbef162892fe24e3
Juan Jose Farina <juanjosefarina.jjf@gmail.com>
2026-04-26 04:13:05 +0000

    fix(config): invert cc fail threshold so it fails when there are fewer lloc per cc branch

--- pymetrica/metric_calculators/cyclomatic_complexity/get_cc.py
+++ pymetrica/metric_calculators/cyclomatic_complexity/get_cc.py
@@ -26,7 +26,7 @@ def cc(
     exit_status = (
         0
         if Configuration.cc_fail_threshold == 0
-        or cc_metric.results.lloc_per_cc <= Configuration.cc_fail_threshold
+        or cc_metric.results.lloc_per_cc >= Configuration.cc_fail_threshold
         else 1
     )
     if exit_status > 0:
